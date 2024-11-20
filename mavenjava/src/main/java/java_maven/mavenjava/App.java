eb57c0b887e0d486492ed78c3093f560330780e9
santosh <santosh080305.ms@gmail.com>
2024-11-20 14:59:25 +0530

    hi

---
+++ mavenjava/src/main/java/java_maven/mavenjava/App.java
@@ -0,0 +1,13 @@
+package java_maven.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "M SANTOSH - 22BD1A0534" );
+    }
+}
